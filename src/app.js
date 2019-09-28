23cc7047086305681ae1b2d6acacdb0a271a22df
Deepak Guptha <vzan2012@gmail.com>
2019-09-28 21:04:03 +0530

    feat(user): Removing post

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", getPosts);
 // Listen for Add Post
 document.querySelector(".post-submit").addEventListener("click", submitPost);
 
+// Listen for Delete Post
+document.querySelector("#posts").addEventListener("click", deletePost);
+
 // Get Posts
 function getPosts() {
   http
@@ -25,15 +28,30 @@ function submitPost(e) {
     body
   };
 
-
   http
     .post("http://localhost:3000/posts", data)
     .then(data => {
-      ui.showAlert("Post Added","alert alert-success");
+      ui.showAlert("Post Added", "alert alert-success");
       ui.clearFields();
       getPosts();
     })
     .catch(err => console.log(err));
 
   e.preventDefault();
+}
+
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+
+    http
+      .delete(`http://localhost:3000/posts/${id}`)
+      .then(data => {
+        ui.showAlert("Post Removed", "alert alert-success");
+        getPosts();
+      })
+      .catch(err => console.log(err));
+  }
 }
